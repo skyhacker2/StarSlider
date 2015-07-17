6d83426dfb2a14da38e47996606d697bc67336f7
Eleven Chen <skyhacker@126.com>
2015-07-17 11:53:16 +0800

    Initial Commit

---
+++ StarSlider/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StarSlider
+//
+//  Created by Eleven Chen on 15/7/17.
+//  Copyright (c) 2015年 Eleven. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
